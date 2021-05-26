13e7594bd58806e75846e414344e1ff5ab743acb
Mateusz Małagocki <mateusz.malagocki.500@guest.p.lodz.pl>
2021-05-26 09:42:55 +0200

    Working? (WIHTOUT GUI)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sympy import *
+import inspect
 import numpy as np
 from numpy import linalg as LA
 import math
@@ -13,27 +14,16 @@ from decimal import Decimal
 import numdifftools as nd
 
 ########################## GLOBALS #############################
-'''
 a = Symbol('a')
 b = Symbol ('b')
-c = Symbol('c')
-d = Symbol ('d')
-e = Symbol('e')
-f = Symbol ('f')
-g = Symbol('h')
-h = Symbol ('h')
-i = Symbol('i')
-j = Symbol ('j')
-'''
 results = []
 how_many_uknowns = 0
 ######################### CONVERSION #############################
 def convert_function(fun):
     letter = 'a'
     local_range = range(0 ,how_many_uknowns)
-    for ik in local_range:
-        fun = fun.replace( chr(ord(letter) + ik), "x[" + str(ik) + "]" )
-    fun =  lambda x:fun
+    for i in local_range:
+        fun = fun.replace( chr(ord(letter) + i), "x[" + str(i) + "]" )
     return fun
 
 def convert_values(values_string):
@@ -44,174 +34,155 @@ def convert_values(values_string):
     values_array.append(float(values_string[separator+1:]))
     return values_array
 
+def get_derivatives(func):
+    arg_symbols = symbols(inspect.getargspec(func).args)
+    sym_func = func(*arg_symbols)
+
+    return [lambdify(arg_symbols, sym_func.diff(a)) for a in arg_symbols]
+
 ########################## DERIVATIVES #########################
 class gradient:
-    def __init__(self, function, value, letter):
+    def __init__(self, function):
+        function = sympify(function, evaluate = False)
         self.function = function
-        self.value = value
-        self.letter = letter
-        self.derivative = 0
-        self.d_fun = 0
-        self.direction = self.calculate_gradient()
+        self.d_fun = []
+        self.calculate_derivative()
 
     def calculate_derivative(self):
+        letter = 'a'
         function_str = str(self.function)
-        if (function_str.find(self.letter , 0) != -1):
-            xprime = self.function.diff(self.letter)
-            self.derivative = xprime
-            self.d_fun = xprime
-            return 0
-
-        else: 
-            return 0
-
-    def calculate_gradient(self):
-        result = 0
-        self.calculate_derivative()
-        letter_value = self.value
-        letter_value = str(letter_value)
-        self.derivative = str(self.derivative)
-        self.derivative = self.derivative.replace( " ", "" )
-        self.derivative = self.derivative.replace(self.letter, letter_value)
-        #print("self.derivative ", self.derivative)
-        result = eval(self.derivative)
-        return result
-    
-    def get_direction(self):
-        return self.direction
+        local_range = range(0 ,how_many_uknowns)
+        for i in local_range:
+            current_letter = chr(ord(letter) + i)
+            if (function_str.find(current_letter, 0) != -1):
+                xprime = self.function.diff(current_letter)
+                self.d_fun.append(str(xprime))
+            else: 
+                print("Something went wrong")
     
     def get_derivative_fun(self):
         return self.d_fun
 
 ########################## FLETCHER-REEVES #########################  
 class Fletcher_Reeves:
-    def __init__(self, points, e, fun, values, n_order):
-        print("sucess")
+    def __init__(self, points, e, fun, values, n_order, function_str):
         points = convert_values(points)
         return_results = []
-        self.eta = [None] * how_many_uknowns
+        self.eta = 0
         self.points = points
         self.epsylon = e
         self.k = 1
         self.fun = fun
-        self.d_fun = []
-        self.grads = [None] * how_many_uknowns
+        self.derivative = []
         self.values = values
+        self.function_str = function_str
 
-        #print("init values ", self.values)
         local_range = range(0 ,how_many_uknowns)
         for ik in local_range:
             self.values.append(None)
-        self.ds = [None]*how_many_uknowns
+        self.d = [None]*how_many_uknowns
         self.calculate()
         
     def calculate(self):
-        letter = 'a'
         local_range = range(0 ,how_many_uknowns)
-        fun =  lambda x:2*x[0]**2 + 3*x[1]**2
-        grad2 = nd.Gradient(fun)([self.values[0], self.values[1]])
-        for ik in local_range:
-            grad = gradient(self.fun, self.values[ik],"x[" + str(ik) + "]" )
-            self.grads[ik] = gradient.get_derivative_fun(grad)
-            d = -gradient.get_direction(grad)
-            self.ds[ik] = d
-            self.d_fun.append(gradient.get_derivative_fun(grad))
-
+        fun =  lambda x: eval(self.fun)
+        d = -nd.Gradient(fun)([self.values[0], self.values[1]])
+        self.d = d
+        grad = gradient(self.function_str)
+        self.derivative = gradient.get_derivative_fun(grad)
+        local_range = range(0 ,how_many_uknowns)
+        for i in local_range:        
+            self.derivative[i] = convert_function(self.derivative[i])
         range_begin = 0
         range_end = 0
         norm = 10
 
         range_begin = self.points[0] + 50
         range_end = self.points[1] - 50
-        combined_string = self.d_fun[0] + self.d_fun[1]
-        x0 = 0.0
         while norm > self.epsylon:
-            str_fun = self.create_new_fun(combined_string, letter)
-            str_fminfun = self.create_new_fminmsearch(combined_string, letter)
+            str_vals = self.find_values_for_min(self.derivative)
+            #str_fminfun = self.create_new_fminmsearch(self.derivative)
+            str_fun = self.createn_new_fun(self.function_str ,str_vals)
             dich = dichotomy(str_fun, range_begin, range_end)
             min_alpha = dichotomy.get_results(dich)
             min_alpha = np.average(min_alpha, axis = 0)
-            min_alpha = 0.00001
-            self.calculate_value_in_point(min_alpha)
-            norm = math.sqrt(pow(self.ds[0], 2) + pow(self.ds[1], 2)) 
-            self.calculate_next_argument(min_alpha)
-            self.count_eta()
-            self.count_next_d()
+            self.calculate_new_value(min_alpha)
+            norm = math.sqrt(pow(self.d[0], 2) + pow(self.d[1], 2)) 
+            print(norm)
+            self.count_eta(fun)
+            self.count_next_d(fun)
 
         return 1
 
-    def count_next_d(self):
+    def createn_new_fun(self, fun, vals):
+        letter = 'a'
+        local_range = range(0 ,how_many_uknowns)
+        for i in local_range:
+            fun = fun.replace( chr(ord(letter) + i), vals[i] )
+        return fun        
+        
+        
+    def count_next_d(self, fun):
         local_range = range(0 ,how_many_uknowns)
         letter = 'a'
-        for i in local_range:
-            letter_value = self.values[i]
-            letter_value = str(letter_value)
-            derivative = str(self.grads[i])
-            derivative = derivative.replace( " ", "" )
-            derivative = derivative.replace(chr(ord(letter) + i), letter_value)
-            grad_value = eval(derivative)
-            dk1 = -grad_value + self.eta[i]*self.ds[i]
-            self.ds[i] = dk1
+        xk_plus_1_value = nd.Gradient(fun)([self.values[2], self.values[3]])
+        next_d = -xk_plus_1_value + self.eta * self.d
+        self.d = next_d
 
-    def count_eta(self):
+    def count_eta(self, fun):
         local_range = range(0 ,how_many_uknowns)
-        letter = 'a'
         for i in local_range:
-            #fun =  lambda x:2*x[0]**2 + 3*x[1]**2
-            xk_value = nd.Gradient(self.fun)([self.values[0], self.values[1]])
-            xk_plus_1_value = nd.Gradient(self.fun)([self.values[2], self.values[3]])
+            xk_value = nd.Gradient(fun)([self.values[0], self.values[1]])
             xk_value = np.array(xk_value)
+            xk_value_product = np.dot(xk_value, xk_value)
+            
+            xk_plus_1_value = nd.Gradient(fun)([self.values[2], self.values[3]])
             xk_plus_1_value = np.array(xk_plus_1_value)
             xk_product_plus_1 = np.dot(xk_plus_1_value, xk_plus_1_value)
-            single_eta = np.divide(xk_product_plus_1, np.dot(xk_value, xk_value))
-            self.eta[i] = single_eta
+            
+            single_eta = np.divide(xk_product_plus_1, xk_value_product)
+            self.eta = single_eta
 
     def calculate_next_argument(self, min_alpha):
         local_range = range(0 ,how_many_uknowns)
         for i in local_range:
             prev_val = self.values[i]
             self.values[i + how_many_uknowns] = prev_val
-            next_arg = self.values[i] + min_alpha*self.ds[i]
+            next_arg = self.values[i] + min_alpha*self.d[i]
             self.values[i] = round(next_arg, 2)
     
-    def calculate_value_in_point(self, min_alpha):
+    def calculate_new_value(self, min_alpha):
         local_range = range(0 ,how_many_uknowns)
-        letter = 'a'
+        function_str = self.derivative
         for i in local_range:
-            function_str = str(self.d_fun[i])
-            new_value = self.values[i] + min_alpha*self.ds[i]
-            function_str = function_str.replace( chr(ord(letter) + i), str(new_value) )
-            function_str = function_str.replace( " ", "" )
-            value = compile(function_str, "<string>", "eval")
-            value = eval(value)
+            self.values[i + how_many_uknowns] = self.values[i]
+            value = self.values[i] + min_alpha*self.d[i]
             self.values[i] = value
 
-    def create_new_fun(self, fun, letter):
+    def find_values_for_min(self, fun):
         function_str = str(fun)
+        results = [0] * how_many_uknowns
         local_range = range(0 ,how_many_uknowns)
         for i in local_range:
-            if (function_str.find(chr(ord(letter) + i), 0) != -1):
-                #print(" create_new_fun.values[i] => ",self.values[i])
+            if (function_str.find("x[" + str(i) + "]", 0) != -1):
                 letter_value = str(self.values[i])
-                d_value = str(self.ds[i])
-                new_value = letter_value + "+" + "x" + "*" + d_value
-                fun = str(fun)
-                fun = str(fun.replace(chr(ord(letter) + i), new_value))
+                d_value = str(self.d[i])
+                results[i] = letter_value + "+" + "alpha" + "*" + d_value
             else: 
                 print('Couldn"t create new function')
                 return 0
-        return fun
+        return results
 
-    def create_new_fminmsearch(self, fun, letter):
+    def create_new_fminmsearch(self, fun):
         function_str = str(fun)
         local_range = range(0 ,how_many_uknowns)
         for i in local_range:
-            if (function_str.find(chr(ord(letter) + i), 0) != -1):
+            if (function_str.find("x[" + str(i) + "]", 0) != -1):
                 letter_value = str(self.values[i])
-                d_value = str(self.ds[i])
+                d_value = str(self.d[i])
                 new_value = letter_value + "+" + "x" + "*" + d_value
                 fun = str(fun)
-                fun = str(fun.replace(chr(ord(letter) + i), new_value))
+                fun = str(fun.replace("x[" + str(i) + "]", new_value))
                 fun = str(fun.replace("**", "^"))
             else: 
                 print('Couldn"t create new function')
@@ -290,7 +261,7 @@ class dichotomy:
 
     def f(self, x):
         function_str = str(self.function)
-        function_str = function_str.replace( "x", str(x) )
+        function_str = function_str.replace( "alpha", str(x) )
         function_str = function_str.replace( " ", "" )
         code = compile(function_str, "<string>", "eval")
         return eval(code)
@@ -314,11 +285,11 @@ parameters = input()
 function_str = "2*a**2 + 3*b**2"
 points = "1, 2" 
 e = "1"
-parameters = '1,3'
+values = '1,3'
 how_many_uknowns = 2
 eng = matlab.engine.start_matlab ()
 eng.optimset('Display', 'off');
 
 fun = convert_function(function_str)
-parameters = convert_values(parameters)
-FR = Fletcher_Reeves(points, float(e), fun, parameters, how_many_uknowns)
+values = convert_values(values)
+FR = Fletcher_Reeves(points, float(e), fun, values, how_many_uknowns, function_str)
